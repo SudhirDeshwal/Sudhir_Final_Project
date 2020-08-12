ae349112fa7787b24bf1dd4ddb20cfb6904c98df
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-12 03:12:10 -0400

    added new class

---
+++ Classes/CrudOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Sudhir_Final_Project.Classes
+{
+    class CrudOperation
+    {
+
+    }
+}
