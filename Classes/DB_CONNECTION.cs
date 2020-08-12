b8cb2e7c2034d2c7596771f6f5448098c0cb8247
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-12 02:45:29 -0400

    design changed

---
+++ Classes/DB_CONNECTION.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudhir_Final_Project.Classes
+{
+    class DB_CONNECTION
+    {
+
+
+    }
+}
