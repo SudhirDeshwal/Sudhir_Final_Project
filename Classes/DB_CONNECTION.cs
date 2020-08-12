e187c4d0514bec162e59a800a9cea3c6ac906417
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-12 03:05:26 -0400

    added refernce and db connection

--- Classes/DB_CONNECTION.cs
+++ Classes/DB_CONNECTION.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Sudhir_Final_Project.Classes
 {
     class DB_CONNECTION
     {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sudhir_project");
 
+        // get the connection
+        public MySqlConnection getConnection
+        {
+            get
+            {
+                return connection;
+            }
+        }
+
+
+        // open the connection
+        public void openConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+
+        // close the connection
+        public void closeConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
 
     }
 }
