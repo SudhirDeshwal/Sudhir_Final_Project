1f988bfa92b0ce5caae00e43aacf185f6db2f7d7
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-12 03:14:48 -0400

    added functions

--- Classes/CrudOperation.cs
+++ Classes/CrudOperation.cs
@@ -11,5 +11,46 @@ namespace Sudhir_Final_Project.Classes
     class CrudOperation
     {
 
+        //importing connection.
+        DB_CONNECTION connection = new DB_CONNECTION();
+
+
+        //exe. query
+        public Boolean ExecQuery(MySqlCommand command)
+        {
+            command.Connection = connection.getConnection;
+
+            connection.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                connection.closeConnection();
+                return true;
+            }
+            else
+            {
+                connection.closeConnection();
+                return false;
+            }
+        }
+
+        // get data
+        public DataTable getData(MySqlCommand command)
+        {
+
+            command.Connection = connection.getConnection;
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+
+            DataTable table = new DataTable();
+
+            adapter.Fill(table);
+
+            return table;
+        }
+
+
+
+
     }
 }
